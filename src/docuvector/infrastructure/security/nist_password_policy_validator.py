9727a329be76ed719256cd6e368fc6421af60312
artuartuartu <arthur.andrade2007@sempreceub.com>
2026-06-14 10:35:26 -0300

    style: fix missing newline at end of file

--- src/docuvector/infrastructure/security/nist_password_policy_validator.py
+++ src/docuvector/infrastructure/security/nist_password_policy_validator.py
@@ -106,4 +106,3 @@ class NistPasswordPolicyValidator:
 
         if comparable_token in plain_password.lower():
             raise ValidationError("Senha não pode conter o seu identificador de usuário.")
-        
